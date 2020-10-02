f162462ebc56059c157715780240dab843c397b2
Liu Fengyun <liu@fengy.me>
2020-10-02 16:52:58 +0200

    Update actions

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const exec = require('@actions/exec');
 
+const command = core.getInput('command');
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    await exec.exec(command);
   } catch (error) {
     core.setFailed(error.message);
   }
